f735288b4a54e79c3333f6fdb4629f1098f173aa
gustaedit <gustaaa.edit@gmail.com>
2023-10-01 13:28:02 -0300

    push

---
+++ estudo pessoal/js/push/js.js
@@ -0,0 +1,12 @@
+var arr = [luiz, ubirajara,rauan,1,2,3,4];
+function attArr(){
+    arr.push('str empurrada');
+    return arr;
+}
+console.log(attArr());
+
+////////////////////////////////////////////for
+
+for(var num = 1;numero<= 30;numero++ ){
+    console.log(numero)
+}
